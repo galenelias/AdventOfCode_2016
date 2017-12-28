c0756d4f215fa410735a914899e55c1d76d1d9d8
Galen Elias <gelias@microsoft.com>
2017-12-28 15:29:48 -0800

    Day 16 Rust (part 2)

--- src/Day16/mod.rs
+++ src/Day16/mod.rs
@@ -4,12 +4,7 @@ fn checksum(input : &[char]) -> Vec<char> {
 	input.windows(2).step_by(2).map(|window| if window[0] == window[1] { '1' } else { '0' }).collect::<Vec<_>>()
 }
 
-pub fn solve() {
-	let stdin = io::stdin();
-	let mut data = stdin.lock().lines().next().unwrap().unwrap().to_string();
-
-	let target_length = 272;
-
+fn fill_and_compute_checksum(mut data : String, target_length : usize) -> String {
 	// Dragon curve up to target length
 	while data.len() < target_length {
 		let mut reverse_half = data.chars().map(|ch| if ch == '0' { '1' } else { '0' }).collect::<Vec<char>>();
@@ -26,5 +21,13 @@ pub fn solve() {
 		cs = checksum(&cs);
 	}
 
-	println!("Part 1: {}", cs.iter().collect::<String>());
+	cs.iter().collect::<String>()
+}
+
+pub fn solve() {
+	let stdin = io::stdin();
+	let data = stdin.lock().lines().next().unwrap().unwrap().to_string();
+
+	println!("Part 1: {}", fill_and_compute_checksum(data.clone(), 272));
+	println!("Part 2: {}", fill_and_compute_checksum(data, 35651584));
 }
